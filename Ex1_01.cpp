9133583d47c718d9c7ff9aefb69fe7b199fac3cd
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-13 12:56:05 +0430

    Adding multi-line comments

--- Ex1_01.cpp
+++ Ex1_01.cpp
@@ -9,5 +9,7 @@ int main(){
     // Defining answer with value 42
     int answer {42};
     cout << "The answer to life, the universe and everything is " << answer << endl;
+    /* This comment is
+       over two lines. */
     return 0;
 }
